2a531a3a33a8361b647c6d09e726987e57513743
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-07 17:48:18 +0100

    Create aaa accounting network enabled.js

---
+++ Cisco/Compliance/Js/aaa accounting network enabled.js
@@ -0,0 +1,8 @@
+function check(device) {
+  var config = device.get("runningConfig");
+  if (config.includes("aaa accounting network") {
+    return CONFORMING;
+  }
+  else {
+    return NONCONFORMING;
+}
