d0af243b9ad0901a0e2d4121cf117e547f16f0b4
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-07 17:25:22 +0100

    Create aaa authentication login enabled.js

---
+++ Cisco/Compliance/Js/aaa authentication login enabled.js
@@ -0,0 +1,8 @@
+function check(device) {
+  var config = device.get("runningConfig");
+  if (config.includes("aaa authentication login") {
+    return CONFORMING;
+  }
+  else {
+    return NONCONFORMING;
+}
