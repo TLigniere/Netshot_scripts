6aa3004f19c0483d4c42e9847bbc6504bf1ae2e2
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-07 17:48:54 +0100

    Create aaa accounting system enabled.js

---
+++ Cisco/Compliance/Js/aaa accounting system enabled.js
@@ -0,0 +1,8 @@
+function check(device) {
+  var config = device.get("runningConfig");
+  if (config.includes("aaa accounting system") {
+    return CONFORMING;
+  }
+  else {
+    return NONCONFORMING;
+}
