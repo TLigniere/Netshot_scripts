5501dddb0ecf991fca33740cdf4afbe8c5a5c8e8
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-07 17:47:05 +0100

    Create aaa accounting exec enabled.js

---
+++ Cisco/Compliance/Js/aaa accounting exec enabled.js
@@ -0,0 +1,8 @@
+function check(device) {
+  var config = device.get("runningConfig");
+  if (config.includes("aaa authentication enable") {
+    return CONFORMING;
+  }
+  else {
+    return NONCONFORMING;
+}
