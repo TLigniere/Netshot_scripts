0be4d6770b515c80ca518325c877e0375f656bec
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-14 16:49:18 +0100

    Update no service dhcp.js

--- Cisco/Compliance/Js/no service dhcp.js
+++ Cisco/Compliance/Js/no service dhcp.js
@@ -1,12 +1,12 @@
 function check(device) {
     var config = device.get('runningConfig');
-    var regex= /no service dhcp/gm;
+    var regex= /^service dhcp/gm;
     var found = config.match(regex);
     
-    if (found==null) {
+    if (found!=null) {
         return NONCONFORMING
     }
-    else {
+    else{
         return CONFORMING
     }
 }
