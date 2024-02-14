8940e8ebb6815c1714e88147426762eafda3e26f
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-14 14:42:41 +0100

    Update login authentication in line tty.js

--- Cisco/Compliance/Js/login authentication in line tty.js
+++ Cisco/Compliance/Js/login authentication in line tty.js
@@ -1,14 +1,15 @@
 function check(device) {
     config= device.get("runningConfig")
     regex = /^line tty/m
-    description_of_int = "login authentication"
+    value = "login authentication"
     Section_OBJ = device.findSections(`${config}`, `${regex}`)
     Section_STR =  (JSON.stringify(Section_OBJ));
 
-    if (Section_STR.includes(`description ${descripton_of_int}`)){
+    if (Section_STR.includes(`${descripton_of_int}`)){
         return CONFORMING
     }
     else {
         return NONCONFORMING
     }
 }
+
