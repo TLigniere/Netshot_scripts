3b953a2cc5b6bec574c5b43fd7f720b13591138f
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-14 14:30:51 +0100

    Create transport input ssh for line vty.js

---
+++ Cisco/Compliance/Js/transport input ssh for line vty.js
@@ -0,0 +1,14 @@
+function check(device) {
+    config= device.get("runningConfig")
+    regex = /^line vty/m
+    description_of_int = "transport input ssh"
+    Section_OBJ = device.findSections(`${config}`, `${regex}`)
+    Section_STR =  (JSON.stringify(Section_OBJ));
+
+    if (Section_STR.includes(`description ${descripton_of_int}`)){
+        return CONFORMING
+    }
+    else {
+        return NONCONFORMING
+    }
+}
