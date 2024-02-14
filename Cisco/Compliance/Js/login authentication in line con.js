5e0daaf286a63aba70e6584187dba89d8f164e32
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-14 14:43:08 +0100

    Update login authentication in line con.js

--- Cisco/Compliance/Js/login authentication in line con.js
+++ Cisco/Compliance/Js/login authentication in line con.js
@@ -1,11 +1,11 @@
 function check(device) {
     config= device.get("runningConfig")
     regex = /^line con/m
-    description_of_int = "login authentication"
+    value = "login authentication"
     Section_OBJ = device.findSections(`${config}`, `${regex}`)
     Section_STR =  (JSON.stringify(Section_OBJ));
 
-    if (Section_STR.includes(`description ${descripton_of_int}`)){
+    if (Section_STR.includes(`${value}`)){
         return CONFORMING
     }
     else {
