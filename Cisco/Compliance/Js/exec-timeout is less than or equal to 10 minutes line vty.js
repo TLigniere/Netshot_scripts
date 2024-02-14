f6ab6edb3d4c9d6539be9772f4a0bd9e8841f071
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-14 15:34:50 +0100

    Create exec-timeout is less than or equal to 10 minutes line vty.js

---
+++ Cisco/Compliance/Js/exec-timeout is less than or equal to 10 minutes line vty.js
@@ -0,0 +1,24 @@
+function check(device) {
+    config= device.get("runningConfig")
+    regex = /^line vty/m
+    snd_regex = /exec-timeout .*/m
+  
+    Section_OBJ = device.findSections(`${config}`, `${regex}`)
+    Section_STR =  (JSON.stringify(Section_OBJ));
+
+    result = Section_STR.match(snd_regex)
+  
+    if (result==null){
+      return CONFORMING
+    }
+  
+    min = Number(result.match(/\d+/))
+    sec = Number(result.match(/\d+$/))
+  
+    if (((min==10) && (sec==0)) | ((min<10)&&(sec<59))) {
+        return CONFORMING
+    }
+    else {
+        return NONCONFORMING
+    }
+}
