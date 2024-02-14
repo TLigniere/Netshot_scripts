d5851dc209fe390147ac2652be2e44737f8f32c4
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-14 15:48:10 +0100

    Update service password-encryption enabled.js

--- Cisco/Compliance/Js/service password-encryption enabled.js
+++ Cisco/Compliance/Js/service password-encryption enabled.js
@@ -2,20 +2,11 @@ function check(device) {
     var config = device.get('runningConfig');
     var regex= /^service password-encryption$/gm;
     var found = config.match(regex);
-    var result
     
     if (found==null) {
-        result=false
+        return NONCONFORMING
     }
     else {
-        result=true
-    }
-
-
-    if (result==true){
-        return CONFORMING;
-    }
-    else {
-        return NONCONFORMING;
+        return CONFORMING
     }
 }
