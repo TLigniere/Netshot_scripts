d1d3df2a122d28b8ac37fd5a595ffa02f0ddc33f
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2024-02-22 13:53:18 +0100

    Update Check interface description.js

--- Cisco/Compliance/Js/Check interface description.js
+++ Cisco/Compliance/Js/Check interface description.js
@@ -1,7 +1,7 @@
 function check(device) {
     config= device.get("runningConfig")
-    regex = /^interface Vlan100/m
-    value = "description VLAN_USERS"
+    regex = /^interface INTERFACE_NAME/m
+    value = "description DESCRIPTION"
     
     Section_OBJ = device.findSections(`${config}`, `${regex}`)
     Section_STR =  (JSON.stringify(Section_OBJ));
